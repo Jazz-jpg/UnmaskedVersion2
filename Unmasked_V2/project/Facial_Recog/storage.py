f5e07ec05f0a43bfc78918c17ebdab1a19b2a261
Jazz-jpg <57850625+Jazz-jpg@users.noreply.github.com>
2023-06-13 15:54:37 -0400

    Update storage.py

--- Unmasked_V2/project/Facial_Recog/storage.py
+++ Unmasked_V2/project/Facial_Recog/storage.py
@@ -4,13 +4,7 @@ import pyrebase
 import os
 
 config = { 
-    "apiKey": "AIzaSyAxV8-iToKuLitUmG48EEkIddvq7iYrN2Y",
-    "authDomain": "facial-recongition-38069.firebaseapp.com",
-    "databaseURL": "https://facial-recongition-38069-default-rtdb.firebaseio.com",
-    "projectId": "facial-recongition-38069",
-    "storageBucket": "facial-recongition-38069.appspot.com",
-    "messagingSenderId": "937313859878",
-    "appId": "1:937313859878:web:25d017bad4c1df1255e9e7"
+   
 
 }
 
