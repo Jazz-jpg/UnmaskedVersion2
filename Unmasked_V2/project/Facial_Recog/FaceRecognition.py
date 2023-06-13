d6f4143f6da88427bc2a2de4cb47e4e3d2ac4535
Jazz-jpg <57850625+Jazz-jpg@users.noreply.github.com>
2023-06-13 15:50:23 -0400

    Update FaceRecognition.py

--- Unmasked_V2/project/Facial_Recog/FaceRecognition.py
+++ Unmasked_V2/project/Facial_Recog/FaceRecognition.py
@@ -8,7 +8,7 @@ import cv2 as cv
 #Abailability: https://www.youtube.com/watch?v=oXlwWbU8l2o, https://github.com/jasmcaus/opencv-course
 
 # Obtain info from FaceTesting
-haar_cascade = cv.CascadeClassifier('C:\Users\Jazz\Desktop\UnmaskedVersion2\Unmasked_V2\project\Facial_Recog\haar_face.xml')
+haar_cascade = cv.CascadeClassifier('')
 
 people = ["Ben Afflek", "Beyonce", "Scarlett Johansson","Ryan Reynolds"]
 # np.load('features.npy')
@@ -18,7 +18,7 @@ people = ["Ben Afflek", "Beyonce", "Scarlett Johansson","Ryan Reynolds"]
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('face_trained.yml')
 #get image needing to be verified
-img = cv.imread(r"C:/Users/Gamer/Desktop/GitRipo/UnmaskedVersion2/Unmasked_V2/project/Facial_Recog/TestingFaces/Validation/Ryan Reynolds/0a8d421e28716e6805ce7a2bd0f3052d.jpg")
+img = cv.imread(r"")
 
 #turn image to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
